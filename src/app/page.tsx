a9103fe4a5a6189edb12b3882c1dead3ccacc376
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2025-06-22 06:18:27 +0300

    simplify design and add system theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-4xl font-bold'>Hello World</h1>
+    <div className='flex flex-col items-center justify-center h-screen dark:bg-black'>
+      <h1 className='text-4xl font-bold text-gray-900 dark:text-white'>
+        Hello World
+      </h1>
     </div>
   );
 }
