8eeb7411564de579b8e02353c483027bedaf7d69
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2025-06-24 11:03:29 +0300

    add text animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
         Stoyan Grozdanov
       </h1>
       <article className='space-y-6 '>
-        <p className='mb-6'>
+        <p className='mb-6 slide-enter'>
           Hey! I&apos;m Stoyan, a software engineer and blockchain / AI investor.
         </p>
-        <p className='flex items-center gap-2 mb-1'>
+        <p className='flex items-center gap-2 mb-1 slide-enter-1'>
           Working at{' '}
           <span className='text-gray-500 flex items-center gap-1 bg-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 px-1.5 py-0.5 rounded-sm cursor-pointer hover:bg-gray-300 transition-colors duration-300 hover:text-gray-900'>
             <Image
@@ -24,7 +24,7 @@ export default function Home() {
             <span>IC Consult</span>
           </span>
         </p>
-        <p className='flex items-center gap-2'>
+        <p className='flex items-center gap-2 slide-enter-1'>
           Creator of{' '}
           <span className='text-gray-500 flex items-center gap-1 bg-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 px-1.5 py-0.5 rounded-sm cursor-pointer hover:bg-gray-300 transition-colors duration-300 hover:text-gray-900'>
             <span
@@ -43,19 +43,19 @@ export default function Home() {
             <span>Digital Finance</span>
           </span>
         </p>
-        <p className='mt-4'>
+        <p className='mt-4 slide-enter-2'>
           I find passion and enjoyment in building things from scratch and
           turning raw ideas into real, working products. I love to brainstorm
           and break down problems to their core as simple solutions scale,
           complex ones fail.
         </p>
-        <p className='mt-4'>
+        <p className='mt-4 slide-enter-3'>
           I write blog posts about software engineering, Web3, and thoughts on
           AI - sharing what I learn and where I think things are headed. I&apos;m
           also building <b>Digital Finance</b> to rethink how we track and grow
           our money.
         </p>
-        <p className='mt-4'>
+        <p className='mt-4 slide-enter-4'>
           Outside of programming and investing, I enjoy traveling, off-roading,
           finding good food, spending time in nature and exercising. I share
           some of those moments on{' '}
