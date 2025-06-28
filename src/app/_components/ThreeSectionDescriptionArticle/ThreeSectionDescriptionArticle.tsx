504db2eceb485382273bb79594e934857ac11f50
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2025-06-28 08:23:35 +0300

    three section description article design tweaks

--- src/app/_components/ThreeSectionDescriptionArticle/ThreeSectionDescriptionArticle.tsx
+++ src/app/_components/ThreeSectionDescriptionArticle/ThreeSectionDescriptionArticle.tsx
@@ -19,9 +19,9 @@ export default function ThreeSectionDescriptionArticle({
   return (
     <Animate
       animationName='fadeInUp'
-      className='flex justify-evenly px-24 py-20 lg:px-38 lg:py-25 max-lg:flex-col max-lg:items-center max-lg:px-8 max-lg:py-15'
+      className='flex gap-12 px-24 py-20 lg:px-48 lg:py-25 xl:px-64 xl:gap-16 max-lg:flex-col max-lg:items-center max-lg:px-8 max-lg:py-15 max-lg:gap-6'
     >
-      <section className='w-[30%] max-lg:w-full max-lg:text-center'>
+      <section className='flex-1 max-lg:w-full max-lg:text-center'>
         <h3 className='text-gray-500 m-0 text-sm'>Product</h3>
         <h4 className='mt-1 mb-5 font-bold text-[0.95rem]'>{product}</h4>
         <h3 className='text-gray-500 m-0 text-sm'>Period</h3>
@@ -30,11 +30,11 @@ export default function ThreeSectionDescriptionArticle({
           {ended_at ? format(ended_at, 'MMMM do yyyy') : 'now'}
         </h4>
       </section>
-      <section className='w-[30%] max-lg:w-full max-lg:text-center'>
+      <section className='flex-1 max-lg:w-full max-lg:text-center'>
         <h3 className='text-gray-500 m-0 text-sm'>Concept</h3>
         <h4 className='mt-1 mb-5 font-bold text-[0.95rem]'>{concept}</h4>
       </section>
-      <section className='w-[60%] pl-12 max-lg:w-4/5 max-lg:text-center max-lg:p-0'>
+      <section className='flex-1 max-lg:w-full max-lg:text-center'>
         <h3 className='text-gray-500 m-0 text-sm'>Tech Stack</h3>
         <h4 className='mt-1 mb-5 font-bold text-[0.95rem]'>
           {techStack && techStack.join(', ')}
