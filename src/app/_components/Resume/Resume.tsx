f38d937b3d1ef6d947340e10a0980fe2d62c3a65
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2025-07-29 13:58:59 +0300

    center the resume icon

--- src/app/_components/Resume/Resume.tsx
+++ src/app/_components/Resume/Resume.tsx
@@ -7,7 +7,7 @@ type ResumeLink = { link: string };
 export default function Resume({ link }: ResumeLink) {
   return (
     <Link target='_blank' rel='noreferrer' href={link}>
-      <div className='dark:shadow-gray-600 animate-fade-in-down-delay-1 p-2 text-center text-sm absolute -translate-x-1/2 [@media(min-width:1020px)]:right-[300px] -translate-y-1/2 left-1/2 [@media(min-width:1400px)]:right-[750px] lg:left-auto -top-3 lg:top-48 z-[1] shadow-md flex flex-col font-semibold hover:cursor-pointer'>
+      <div className='dark:shadow-gray-600 animate-fade-in-down-delay-1 p-2 text-center text-sm absolute -translate-x-1/2 [@media(min-width:1020px)]:right-[300px] -translate-y-1/2 left-1/2 [@media(min-width:1400px)]:right-[750px] lg:left-auto -top-3 lg:top-48 z-[1] shadow-md flex flex-col font-semibold hover:cursor-pointer items-center'>
         <FontAwesomeIcon
           icon={faFileArrowDown}
           beat
