19631c3ce15afdaff860b99cfaec2bd06e5bb251
Stoyan Grozdanov <st.grozdanov@abv.bg>
2026-05-06 09:13:51 +0300

    fix logo designs in the project page

--- src/app/_components/ProjectCard/ProjectCard.tsx
+++ src/app/_components/ProjectCard/ProjectCard.tsx
@@ -19,14 +19,16 @@ export default function ProjectCard({ work, collection }: ProjectCardProps) {
       href={`/${collection}/${work.product}`}
       className='group mt-6 flex items-center justify-between p-4 rounded-lg transition-all duration-600 '
     >
-      <article className='flex items-center'>
-        <Image
-          src={work.logo}
-          alt='logo'
-          width={120}
-          height={120}
-          className='filter grayscale group-hover:grayscale-0 transition-all duration-600'
-        />
+      <article className='flex items-center gap-4'>
+        <div className='w-24 h-16 shrink-0 flex items-center justify-center'>
+          <Image
+            src={work.logo}
+            alt='logo'
+            width={120}
+            height={120}
+            className='filter grayscale group-hover:grayscale-0 transition-all duration-600 object-contain w-full h-full'
+          />
+        </div>
         <div className='flex flex-col'>
           <div className='text-gray-800 dark:text-white group-hover:text-black dark:group-hover:text-white transition-all duration-600'>
             {work.product}
